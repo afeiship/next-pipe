2c6909c33df0759d3dd72e65532f68b4aa1236a8
afeiship <1290657123@qq.com>
2023-04-05 22:04:16 +0800

    refactor: use new impl

--- __tests__/index.spec.js
+++ __tests__/index.spec.js
@@ -1,15 +1,14 @@
-(function () {
-  require('../src');
-  const addOne = (n) => n + 1;
-  const double = (n) => n * 2;
-  const sqrt = (n) => n * n;
+require('../src');
 
-  describe('api.basic test', () => {
-    test('nx.pipe multiple pipe', function () {
-      const pipeFn1 = nx.pipe(addOne, double, sqrt);
-      const pipeFn2 = nx.pipe(double, sqrt, addOne);
-      expect(pipeFn1(10)).toBe(484);
-      expect(pipeFn2(10)).toBe(401);
-    });
+const addOne = (n) => n + 1;
+const double = (n) => n * 2;
+const sqrt = (n) => n * n;
+
+describe('api.basic test', () => {
+  test('nx.pipe multiple pipe', function () {
+    const pipeFn1 = nx.pipe(addOne, double, sqrt);
+    const pipeFn2 = nx.pipe(double, sqrt, addOne);
+    expect(pipeFn1(10)).toBe(484);
+    expect(pipeFn2(10)).toBe(401);
   });
-})();
+});
--- src/index.js
+++ src/index.js
@@ -1,20 +1,19 @@
-(function () {
-  var global = global || this || window || Function('return this')();
-  var nx = global.nx || require('@jswork/next');
+import nx from '@jswork/next';
 
-  nx.pipe = function () {
-    var fns = nx.slice(arguments);
-    if (fns.length === 0) return nx.stubValue;
-    if (fns.length === 1) return fns[0];
+nx.pipe = function () {
+  var fns = nx.slice(arguments);
+  if (fns.length === 0) return nx.stubValue;
+  if (fns.length === 1) return fns[0];
 
-    return fns.reduceRight(function (fn1, fn2) {
-      return function () {
-        return fn1(fn2.apply(null, arguments));
-      };
-    });
-  };
+  return fns.reduceRight(function (fn1, fn2) {
+    return function () {
+      return fn1(fn2.apply(null, arguments));
+    };
+  });
+};
 
-  if (typeof module !== 'undefined' && module.exports) {
-    module.exports = nx.pipe;
-  }
-})();
+if (typeof module !== 'undefined' && module.exports) {
+  module.exports = nx.pipe;
+}
+
+export default nx.pipe;
